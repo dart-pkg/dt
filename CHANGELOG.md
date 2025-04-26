97bdb525d9439fea5c6ee4bf4bd971a430c3b885
JavaCommons Technologies <javacommons@gmail.com>
2025-04-26 21:19:05 +0900

    dt: Update dependencies

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,20 @@
 ## 2025.425.1054
 
 - Initial release
+
+## 2025.426.2118
+
+- Update dependencies
+
+```
+--- a/pubspec.yaml
++++ b/pubspec.yaml
+-version: 2025.425.1054
++version: 2025.426.2118
+-  run: ^2025.424.1840
+-  sys: ^2025.425.243
++  std: ^2025.426.1637
++  sys: ^2025.426.2033
+-  output: ^1.0.7
++  output: ^2025.426.2027
+```
--- bin/main.dart
+++ bin/main.dart
@@ -3,11 +3,10 @@
 import 'dart:core';
 import 'package:sys/sys.dart' as sys;
 import 'package:args/args.dart' as args;
-import 'package:run/run.dart' as run__;
+import 'package:std/command_runner.dart' as std__;
+import 'package:std/misc.dart' as std__;
 
-// 【Dart】コマンドライン引数解析ライブラリargsを試す https://zenn.dev/slowhand/articles/7ca7a2250b65a3
 main(List<String> $args) async {
-  //dump(misc.isInDebugMode, 'isInDebugMode');
   if (sys.isInDebugMode) {
     $args = [
       'script',
@@ -16,17 +15,11 @@ main(List<String> $args) async {
       'b',
       'cハロー©',
     ];
-    //$args = ['run', r'D:\home11\dart\hello\bin\my.api-call.dart'];
-    //$args = ['run', r'D:\home11\dart\hello\bin\my.api-call.dart', 'abc'];
-    //$args = ['run'];
   }
   // try {
   var $parser = args.ArgParser();
   /*var $command = */
   $parser.addCommand('run');
-  //$command.addFlag('all', abbr: 'a');
-  // $parser.addCommand('scr');
-  // $parser.addCommand('script');
   $parser.addCommand('fix');
   $parser.addCommand('dep');
   $parser.addCommand('deps');
@@ -57,16 +50,7 @@ main(List<String> $args) async {
       {
         await projDir($commandResults);
       }
-    // case 'scr':
-    // case 'script':
-    //   {
-    //     await script($commandResults);
-    //   }
   }
-  // } catch ($e, $stacktrace) {
-  //   dump($e, 'Exception');
-  //   dump($stacktrace, 'Stacktrace');
-  // }
 }
 
 Future<void> run(args.ArgResults $commandResults) async {
@@ -74,22 +58,15 @@ Future<void> run(args.ArgResults $commandResults) async {
     throw 'File name count is ${$commandResults.rest.length}: ${$commandResults.rest}';
   }
   String $filePath = $commandResults.rest[0];
-  $filePath = $filePath.replaceAll('/', '\\');
+  //$filePath = $filePath.replaceAll('/', '\\');
   $filePath = sys.pathFullName($filePath);
   String $libDir = sys.pathDirectoryName($filePath);
   String $cwd = sys.getCwd();
-  //String $fn = sys.pathFileName($libDir);
-  //echo($fn, r'$fn');
-  // if ($fn == 'bin' || $fn == 'lib' || $fn == 'test') {
-  //   sys.setCwd(sys.pathDirectoryName($libDir));
-  // } else {
-  //   sys.setCwd($libDir);
-  // }
   sys.setCwd($libDir);
-  final run = run__.Run(useUnixShell: false);
-  await run.$('specgen');
+  final run = std__.CommandRunner(useUnixShell: false);
+  await run.run('specgen');
   sys.setCwd($cwd);
-  await run.$$(
+  await run.run$(
     'dart',
     arguments: [$filePath, ...$commandResults.rest.toList().sublist(1)],
     //autoQuote: false,
@@ -97,50 +74,27 @@ Future<void> run(args.ArgResults $commandResults) async {
 }
 
 Future<void> fix(args.ArgResults $commandResults) async {
-  // await sys.runAsync$(['dart', 'fix', '--apply'], useBash: true);
-  // await sys.runAsync$(['dart', 'format', '.'], useBash: true);
-  final run = run__.Run(useUnixShell: true);
-  await run.$('dart fix --apply');
-  await run.$('dart format .');
+  final run = std__.CommandRunner();
+  await run.run('dart fix --apply');
+  await run.run('dart format .');
 }
 
 Future<void> deps(args.ArgResults $commandResults) async {
-  // await sys.runAsync(
-  //   'dart pub deps --no-dev --style list | sed "/^ .*/d"',
-  //   useBash: true,
-  // );
-  final run = run__.Run(useUnixShell: true);
-  await run.$('dart pub deps --no-dev --style list | sed "/^ .*/d"');
+  // final run = run__.CommandRunner(useUnixShell: true);
+  // await run.run('dart pub deps --no-dev --style list | sed "/^ .*/d"');
+  final run = std__.CommandRunner();
+  String result = await run.run(
+    'dart pub deps --no-dev --style list',
+    silent: true,
+  );
+  List<String> lines = std__.textToLines(result);
+  for (int i = 0; i < lines.length; i++) {
+    String line = lines[i];
+    if (line.startsWith(' ')) continue;
+    //print('> $line');
+    print(line);
+  }
 }
-
-// Future<void> script(args.ArgResults $commandResults) async {
-//   if ($commandResults.rest.isEmpty) {
-//     throw 'File name count is ${$commandResults.rest.length}: ${$commandResults.rest}';
-//   }
-//   String $filePath = $commandResults.rest[0];
-//   $filePath = sys.pathFullName($filePath);
-//   String $libDir = sys.pathDirectoryName($filePath);
-//   String $cwd = sys.getCwd();
-//   String $fn = sys.pathFileName($cwd);
-//   echo($fn, r'$fn');
-//   if ($fn == 'bin' || $fn == 'lib' || $fn == 'test') {
-//     sys.setCwd(sys.pathDirectoryName($libDir));
-//   } else {
-//     sys.setCwd($libDir);
-//   }
-//   await sys.runAsync('specgen');
-//   sys.setCwd($cwd);
-//   var spawner = DartSpawner(
-//     directory: io.Directory(sys.pathDirectoryName($libDir)),
-//   );
-//   String $script = sys.readFileString($filePath);
-//   var $spawned = await spawner.spawnDart(
-//     $script,
-//     $commandResults.rest.toList().sublist(1),
-//   );
-//   var $exitCode = await $spawned.exitCode;
-//   io.exit($exitCode);
-// }
 
 Future<void> projDir(args.ArgResults $commandResults) async {
   if ($commandResults.rest.isEmpty) {
